f99d7023cfc012fcd949b621efc7faefdcb394d0
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-11-20 01:47:25 +0000

    Add shop page and secondary shop through pages

---
+++ src/app/shop/[item]/page.tsx
@@ -0,0 +1,100 @@
+import Image from 'next/image';
+import Navbar from '../../../components/Navbar';
+import Footer from '../../../components/Footer'
+import fs from 'fs';
+import path from 'path';
+
+interface ShopPageProps {
+  params: {
+    item: string;
+  };
+}
+
+const ShopItem = async ({ params }: ShopPageProps) => {
+    const { item } = await params;
+
+    console.log("Debug: item =", item); // Debug output
+
+    if (!item) {
+        console.log("Debug: Item is missing"); // Debug output
+        return <div>Error: Shop item name is missing</div>;
+    }
+
+    const imagesDirectory = path.join(process.cwd(), 'public', 'shop', item);
+    
+    console.log("Debug: imagesDirectory =", imagesDirectory); // Debug output
+
+    if (!fs.existsSync(imagesDirectory)) {
+        console.log("Debug: Directory not found"); // Debug output
+        return <div>404 - Shop Item Not Found</div>;
+    }
+
+    let files;
+    try {
+        files = fs.readdirSync(imagesDirectory);
+        console.log("Debug: files =", files); // Debug output
+    } catch (error) {
+        console.error('Error reading directory:', error);
+        return <div>Error loading images</div>;
+    }
+    
+    const images = files.map(file => ({
+        src: `/shop/${item}/${file}`,
+        alt: `${item} image ${file}`,
+    }));
+
+    console.log("Debug: images =", images); // Debug output
+
+    return (
+        <>
+            <div className='bg-white flex flex-col min-h-screen gap-y-7 px-7 pt-7 pb-14'>
+                <Navbar/>
+                <div className='flex-grow flex flex-col pt-7'>
+                    <div className='flex flex-row gap-x-12'>
+                        <div className="grid grid-cols-2 gap-7">
+                            {images.map((image, index) => (
+                                <div key={index} className="relative">
+                                    <Image 
+                                        src={image.src} 
+                                        alt={image.alt} 
+                                        width={420}
+                                        height={525}
+                                        className="object-cover"
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                        <div className='flex flex-col py-8 gap-y-6'>
+                            <div className='flex flex-col gap-y-2'>
+                                <h2 className='text-[#3A3A3A]'>
+                                    TARGET PRACTISE TROUSERS
+                                </h2>
+                                <div className='flex flex-col gap-y-1'>
+                                    <p className='text-[#676767]'>
+                                        100% of the shots you don’t take you miss
+                                    </p>
+                                    <p className='text-[#676767]'>
+                                        but you won’t miss in these
+                                    </p>
+                                    <p className='text-[#676767]'>
+                                        100% guaranteed
+                                    </p>
+                                    <p className='text-[#676767]'>
+                                        okay, maybe 85%
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="h-[1px] w-full bg-[#676767]"></div>
+                            <h2 className='text-[#3A3A3A] whitespace-nowrap text-base'>
+                                MADE TO ORDER - ENQUIRE THROUGH INSTAGRAM
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </>
+    );
+}
+
+export default ShopItem;
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer'
 
@@ -10,45 +11,53 @@ const ShopPage = () => {
                 <div className='flex-grow flex justify-center items-center pt-7'>
                     <div className='grid grid-cols-1  md:grid-cols-2 gap-12'>
                         <div className='flex flex-col gap-y-4'>
-                            <Image
-                                src={'/images/WoolTrousers.png'}
-                                alt='Wool Trouser'
-                                height={450}
-                                width={500}
-                            />
+                            <Link href={'/shop/wool-trousers'}>
+                                <Image
+                                    src={'/images/WoolTrousers.jpg'}
+                                    alt='Wool Trouser'
+                                    height={450}
+                                    width={500}
+                                />
+                            </Link>
                             <h2 className='font-medium text-base text-[#F0F0F0] px-3 py-2 bg-[#FF0000] w-fit rounded-3xl'>
                                 WOOL TROUSER
                             </h2>
                         </div>
                         <div className='flex flex-col gap-y-4'>
-                            <Image
-                                src={'/images/FencingVest.jpg'}
-                                alt='Fencing Vest'
-                                height={450}
-                                width={500}
-                            />
+                            <Link href={'/shop/fencing-vest'}>
+                                <Image
+                                    src={'/images/FencingVest.jpg'}
+                                    alt='Fencing Vest'
+                                    height={450}
+                                    width={500}
+                                />
+                            </Link>
                             <h2 className='font-medium text-base text-[#F0F0F0] px-3 py-2 bg-[#FF0000] w-fit rounded-3xl'>
                                 FENCING VEST
                             </h2>
                         </div>
                         <div className='flex flex-col gap-y-4'>
-                            <Image
-                                src={'/images/HorseRider.jpg'}
-                                alt='HORSE RIDER JACKET'
-                                height={450}
-                                width={500}
-                            />
+                            <Link href={'/shop/horse-rider-jacket'}>
+                                <Image
+                                    src={'/images/HorseRider.jpg'}
+                                    alt='HORSE RIDER JACKET'
+                                    height={450}
+                                    width={500}
+                                />
+                            </Link>
                             <h2 className='font-medium text-base text-[#F0F0F0] px-3 py-2 bg-[#FF0000] w-fit rounded-3xl'>
                             HORSE RIDER JACKET
                             </h2>
                         </div>
                         <div className='flex flex-col gap-y-4'>
-                            <Image
-                                src={'/images/TargetPractise.jpg'}
-                                alt='Wool Trouser'
-                                height={450}
-                                width={500}
-                            />
+                            <Link href={'/shop/target-practise'}>
+                                <Image
+                                    src={'/images/TargetPractise.jpg'}
+                                    alt='Wool Trouser'
+                                    height={450}
+                                    width={500}
+                                />
+                            </Link>
                             <h2 className='font-medium text-base text-[#F0F0F0] px-3 py-2 bg-[#FF0000] w-fit rounded-3xl'>
                                 TARGET PRACTISE TROUSERS
                             </h2>
