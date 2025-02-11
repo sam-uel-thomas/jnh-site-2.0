82fa0f6029fdbcf9ba8f5b7d4e97db7f80b28d49
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2025-02-11 10:42:56 +0000

    Fix deployment errors

--- src/app/[collection]/page.tsx
+++ src/app/[collection]/page.tsx
@@ -1,51 +1,56 @@
 import fs from 'fs';
 import path from 'path';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 import Image from 'next/image';
 
-type PageProps = {
-  params: {
-    collection: string;
-  };
-};
-
-const CollectionPage = async ({ params }: PageProps) => {
+const CollectionPage = async ({ params }: { params: { collection: string } }) => {
   const { collection } = params;
 
   const imagesDirectory = path.join(process.cwd(), 'public', 'collections', collection);
-
+  
   if (!fs.existsSync(imagesDirectory)) {
     return <div>404 - Collection Not Found</div>;
   }
 
   let files = fs.readdirSync(imagesDirectory);
 
-  files = files.filter((file) => file !== '.DS_Store');
+  // Filter out .DS_Store files
+  files = files.filter(file => file !== '.DS_Store');
 
+  // Sort files by number in filename
   const sortedFiles = files.sort((a, b) => {
     const aNumber = parseInt(a.match(/\d+/)?.[0] || '0', 10);
     const bNumber = parseInt(b.match(/\d+/)?.[0] || '0', 10);
     return aNumber - bNumber;
   });
-
-  const images = sortedFiles.map((file) => ({
+  
+  const images = sortedFiles.map(file => ({
     src: `/collections/${collection}/${file}`,
-    alt: `${collection} image ${file}`,
+    alt: `${collection} image ${file}`, // Provide a meaningful alt text
   }));
 
   return (
     <>
-      <div className="bg-white flex flex-col gap-y-7 p-7">
-        <h1 className="border-4 font-bold text-[#3A3A3A] border-[#3A3A3A] rounded-3xl px-3 py-1 w-fit">
+      <div className='bg-white flex flex-col gap-y-7 p-7'>
+        <Navbar/>
+        <h1 className='border-4 font-bold text-[#3A3A3A] border-[#3A3A3A] rounded-3xl px-3 py-1 w-fit'>
           {collection.replace(/-/g, ' ').toUpperCase()}
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-7">
           {images.map((image, index) => (
             <div key={index} className="relative w-full aspect-[4/5]">
-              <Image src={image.src} alt={image.alt} fill className="object-contain" />
+              <Image 
+                src={image.src} 
+                alt={image.alt} 
+                fill
+                className="object-contain"
+              />
             </div>
           ))}
         </div>
       </div>
+      <Footer/>
     </>
   );
 };
